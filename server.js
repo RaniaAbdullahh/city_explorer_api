ece41c039d2b32ea92680a9f1935b8e212d3826b
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-10-30 17:00:13 +0200

    lab9Done

--- server.js
+++ server.js
@@ -17,19 +17,11 @@ app.use(cors());
 //-------------
 const pg = require('pg');
 const request = require('superagent');
+//const axios = require("axios");
 const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 //-------------
-// app.get('/get-locations', (req, res) => {
-//   const location = 'SELECT * FROM location ;';
-//   client.query(location).then(result => {
-//     res.status(200).json(result.rows);
-//   });
-// });
-//-----------
 
-//app.get('/add-location', locationHndler);
-//-------------
 //  home Route
 app.get('/', welcomePage);
 //location  Rout
@@ -41,7 +33,7 @@ app.get('/weather', weatherHandler);
 app.get('/trails', trailsHandler);
 //---------
 app.get('/movies', moviesHandler);
-app.get('/yelp',yelpHandler);
+app.get('/yelp', yelpHandler);
 app.use('*', notFound);
 
 // location constructor
@@ -91,12 +83,12 @@ function Movie(movieObj) {
 
 }
 //yelp constructor 
-function Yelp(yelbObj){
+function Yelp(yelbObj) {
   this.name = yelbObj.name;
-  this.image_url = yelpObj.image_url;
-  this.price = yelpObj.price;
-  this.rating = yelpObj.rating
-  this.url = yeldObj.url;
+  this.image_url = yelbObj.image_url;
+  this.price = yelbObj.price;
+  this.rating = yelbObj.rating;
+  this.url = yelbObj.url;
 }
 
 //helpers functions------------
@@ -126,7 +118,7 @@ function locationHndler(request, response) {
     }
     else {
 
-      console.log('after catch');
+      //console.log('after catch');
       const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOIQ}&q=${city}&format=json`;
       let locationArr;
       superagent.get(url).then(locationData => {
@@ -172,7 +164,7 @@ function trailsHandler(reqeust, response) {
   //console.log(url);
 
 
-  //dont put any thng here !!!!!!!!!!!
+
 
   superagent.get(url).then(trailsData => {
     //console.log(trailsData.body);
@@ -192,7 +184,7 @@ function trailsHandler(reqeust, response) {
 
 
 function moviesHandler(request, response) {
-  let region =request.query.search_query.slice(0,2).toUpperCase();
+  let region = request.query.search_query.slice(0, 2).toUpperCase();
   const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${MOVIES}&region=${region}`;
   superagent.get(url).then(moviesData => {
     //console.log(moviesData.body.results);
@@ -205,19 +197,59 @@ function moviesHandler(request, response) {
   }).catch(console.error);
 }
 
-function yelpHandler(request, response){
-  let location = request.query.search_query;
-  //let url = `https://api.yelp.com/v3/businesses/search?api_key=${YELP}`;
-  let url =`https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972&location=${location}`;
-  superagent.get(url).then(yelpsData => {
+function yelpHandler(request, response) {
+  const region = request.query.search_query;
+  const longitude = request.query.longitude;
+  const latitude = request.query.latitude;
+  const page=request.query.page;
+  let offset=5*(page-1);//for ordering 
+  const url = `https://api.yelp.com/v3/businesses/search`;
+  //passing parameters
+  let queryParemeter={
+    location:region,
+    latitude:latitude,
+    longitude:longitude,
+    api_key:YELP,
+    offset:offset,
+    limit:5,
+    categories:'Restaurants ',
+    format:'json'
+  };
+  superagent.get(url).query(queryParemeter).set('Authorization', `Bearer ${YELP}`).then(yelpsData => {
     console.log(yelpsData.body);
-    let yeld = yelpsData.body.results.map(Data => {
-      //console.log(Data);
-      return new Yeld(Data);
+   let yelps = yelpsData.body.businesses.map((value) => {
+      return (new Yelp(value));
     });
-    response.json(yeld);
-    
-  }).catch(console.error);
+    response.json(yelps);
+  }).catch(() => {
+    response.status(500).send('Sorry,something went wrong');
+  })
+
+  //let url = `https://api.yelp.com/v3/businesses/search?api_key=${YELP}`;
+  //let url =`https://api.yelp.com/v3/businesses/search?api_key=${YELP}&categories=&latitude=37.786882&longitude=-122.399972&location=${location}`;
+
+  // superagent.get(url).then(yelpsData => {
+  //   console.log(yelpsData.body);
+  //   let yelp = yelpsData.body.results.map(Data => {
+  //     //console.log(Data);
+  //     return new Yelp(Data);
+  //   });
+  //   response.json(yelp);
+
+  // }).catch(console.error);
+
+  // axios.get(`https://api.yelp.com/v3/businesses/search`, {headers: { Authorization: `Bearer ${YELP}`}, params: { location: { location },categories: 'Restaurants'}
+  //   .then(yelpsData => {
+  //     console.log(yelpsData.body);
+  //     let yelp = yelpsData.body.results.map(Data => {
+  //       //console.log(Data);
+  //       return new Yelp(Data);
+  //     });
+  //     response.json(yelp);
+
+  //   }).catch(console.error)
+  // })
+
 
 }
 
@@ -229,9 +261,6 @@ function notFound(request, resp) {
   resp.status(404).send('Not found');
 }
 
-function error(request, resp) {
-  resp.status(500).send('Error ! ');
-}
 
 //------------
 //app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
@@ -242,3 +271,7 @@ client.connect().then(() => {
 }).catch(error => {
   console.log('error', error);
 });
+
+
+
+
